271ede38ed91f938bf3cdb661b4c1cec5f16100a
saogiji <ogijiadanusimon@yahoo.com>
2021-08-27 15:41:14 +0100

    Task 8 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alpha;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
