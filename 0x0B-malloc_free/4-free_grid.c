86337ace9a1c8eb5a661fd2778fd485cd75c60b7
saogiji <ogijiadanusimon@yahoo.com>
2021-09-21 13:21:18 +0100

    advanced task of malloc commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees the memory allocate for the grid
+ * 
+ * @grid: grid to free.
+ *
+ * @height: height of the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
