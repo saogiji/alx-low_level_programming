b64b6467be4463b314a28fc8e33ad3f4fd7c26bd
saogiji <ogijiadanusimon@yahoo.com>
2021-09-28 00:16:35 +0100

    tasks commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name of file compiled from
+ *
+ * Return: Always(0) Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
